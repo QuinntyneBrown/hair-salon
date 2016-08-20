69b0bab76a5f11d7cd9032bd1674e62bb67efae6
Quinntyne Brown <quinntynebrown@gmail.com>
2016-08-20 12:43:05 -0400

    routing

---
+++ HairSalon.Web/src/app/routing/app.routing.ts
@@ -0,0 +1,48 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { CanActivateAuthGuard } from './can-activate-auth.service';
+import { UserProfileService } from '../shared/user-profile.service';
+
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home',
+    },
+    {
+        path: 'about',
+        loadChildren: 'app/about/about.module'
+    },
+    {
+        path: 'admin',
+        loadChildren: 'app/admin/admin.module',
+        canActivate: [CanActivateAuthGuard],
+    },
+    {
+        path: 'contact',
+        loadChildren: 'app/contact/contact.module'        
+    },
+    {
+        path: 'gallery',
+        loadChildren: 'app/gallery/gallery.module'
+    },
+    {
+        path: 'home',
+        loadChildren: 'app/home/home.module'
+    },
+    {
+        path: 'services',
+        loadChildren: 'app/services/services.module'
+    },
+    {
+        path: 'whats-new',
+        loadChildren: 'app/whats-new/whats-new.module'
+    }
+];
+
+export const routing = RouterModule.forRoot(routes);
+
+routing.providers.push([
+    CanActivateAuthGuard,    
+    UserProfileService
+]);
---
+++ HairSalon.Web/src/app/routing/can-activate-auth.service.ts
@@ -0,0 +1,24 @@
+﻿import { Injectable } from '@angular/core';
+import {
+    CanActivate,
+    Router,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot
+} from '@angular/router';
+
+@Injectable()
+export class CanActivateAuthGuard implements CanActivate {
+    constructor(private userProfileService: any, private router: Router) { }
+
+    canActivate(
+        next: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ) {
+        if (this.userProfileService.isLoggedIn) {
+            return true;
+        }
+        this.router.navigate(['/login'], { queryParams: { redirectTo: state.url } });
+
+        return false;
+    }
+}
