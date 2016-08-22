f7400030e0d304d2fb95e8aad453c90f3a44d2a1
Quinntyne Brown <quinntynebrown@gmail.com>
2016-08-22 12:18:51 -0400

    router directives

--- HairSalon.Web/src/app/shared/components/nav.component.ts
+++ HairSalon.Web/src/app/shared/components/nav.component.ts
@@ -1,8 +1,10 @@
 import { Component, ChangeDetectionStrategy, Input, OnInit } from "@angular/core";
+import { ROUTER_DIRECTIVES } from "@angular/router";
 
 @Component({
     template: require("./nav.component.html"),
     styles: [require("./nav.component.scss")],
+    directives:[ROUTER_DIRECTIVES],
     selector: "h-nav",
     changeDetection: ChangeDetectionStrategy.OnPush
 })
