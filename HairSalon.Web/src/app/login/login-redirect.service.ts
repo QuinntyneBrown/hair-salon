c6ad3545401e77d9f919ca74e3f8227060d456e2
Quinntyne Brown <quinntynebrown@gmail.com>
2016-08-22 11:20:10 -0400

    login redirect service

--- HairSalon.Web/src/app/login/login-redirect.service.ts
+++ HairSalon.Web/src/app/login/login-redirect.service.ts
@@ -1,6 +1,30 @@
 ﻿import { Injectable } from "@angular/core";
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Injectable()
 export class LoginRedirectService {
+    constructor(
+        private _route: ActivatedRoute,
+        private _router: Router) {
+    }
 
+    loginUrl: string = "/login";
+    lastPath: string;
+    defaultPath: string = "/";
+    setLoginUrl = value => this.loginUrl = value;
+    setDefaultUrl = value => this.defaultPath = value;
+
+    public redirectToLogin = () => {
+        // 1. Set Last Path
+        // 2. Redirect to Login
+    }
+
+    public redirectPreLogin = () => {
+        if (this.lastPath && this.lastPath != this.loginUrl) {
+            // Redirect to last path
+            this.lastPath = "";
+        } else {
+            // Redirect to default path
+        }
+    }
 }
